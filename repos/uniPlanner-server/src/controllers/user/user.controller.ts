3f212a82c371d37aa21bc93aab07b88d1dc7945f
ss <ss@gmail.com>
2022-05-12 20:00:08 +0300

    mongo

--- repos/uniPlanner-server/src/controllers/user/user.controller.ts
+++ repos/uniPlanner-server/src/controllers/user/user.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, HttpException, HttpStatus, Logger } from '@nestjs/common';
+import { Body, Controller, Get, HttpException, HttpStatus, Logger, Param, Post, Put, Query } from '@nestjs/common';
 import { User } from 'src/models/user.model';
 import { MongoService } from 'src/services/mongo.service';
 
@@ -6,14 +6,53 @@ import { MongoService } from 'src/services/mongo.service';
 export class UserController {
     constructor(readonly mongoService: MongoService) {}
 
-    @Get('user')
-    getCourseById(): User {
+    @Get('search/:username')
+    async getUserByName(@Param() params): Promise<User[]> {
         try {
-            Logger.log('Got request to get data');
-            return { id: 'asd', username: 'yoarm', password: '1234' };
+            Logger.log('Got request to get user data from collection');
+            const user: User[] = await this.mongoService.getUserByName("users",params.username) as any;
+            return user;
         } catch (error) {
-            Logger.error(`Failed getting course by id, error: ${error}`)
+            Logger.error(`Failed getting user by name ${params.username}, error: ${error}`)
             throw new HttpException('Forbidden', HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
+
+    @Post('saveUser')
+    async saveUserByCollection(@Body() userdata): Promise<string> {
+        try {
+            Logger.log('Post request to post user data to collection');
+            await this.mongoService.saveItemByCollection("users",  userdata);
+            return `User ${userdata.username} saved successfully`;
+        } catch (error) {
+            Logger.error(`Failed to post data, error: ${error}`)
+            throw new HttpException('Forbidden', HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @Put(':username')
+    async updateUserByName(@Param() username, @Body() userdata): Promise<string> {
+        try {
+            Logger.log('Put request to update user data to collection');
+            await this.mongoService.updateUserByName("users", username , userdata);
+            return `Course ${userdata.username} updated successfully`;
+        } catch (error) {
+            Logger.error(`Failed to put data, error: ${error}`)
+            throw new HttpException('Forbidden', HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @Get('validate')
+    async getUserToValidate(@Query() query: User): Promise<User[]> {
+        try {
+            Logger.log('Got request to get user data from collection');
+            Logger.log(query);
+            const user: User[] = await this.mongoService.GetUserToValidate("users",query.username, query.password) as any;
+            return user;
+        } catch (error) {
+            Logger.error(`Failed getting user by name ${query.username}, error: ${error}`)
+            throw new HttpException('Forbidden', HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
 }
