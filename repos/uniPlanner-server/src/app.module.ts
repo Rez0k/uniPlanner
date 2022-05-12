82554e84780ef2df7fa90d0d0ba4eb79eda9478f
Eran Daniel <eran1eran2@gmail.com>
2022-05-12 18:50:48 +0300

    Merge branch 'mi ain' into courses_table

--- repos/uniPlanner-server/src/app.module.ts
+++ repos/uniPlanner-server/src/app.module.ts
@@ -2,10 +2,12 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { DataController } from './controllers/data/data.controller';
+import { MondayController } from './controllers/data/monday.controller';
+
 
 @Module({
   imports: [],
-  controllers: [AppController, DataController],
+  controllers: [AppController, DataController, MondayController],
   providers: [AppService],
 })
 export class AppModule {}
