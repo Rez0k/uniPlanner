e8399479422276caa9f0f3631a74d963c434b6d8
Ofek Ifergan <ofekifergan@gmail.com>
2022-05-12 21:29:52 +0300

    up

--- repos/uniPlanner-client/src/app/modules/components/courses/course-modal/course-modal.component.ts
+++ repos/uniPlanner-client/src/app/modules/components/courses/course-modal/course-modal.component.ts
@@ -1,16 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { Subscription } from 'rxjs';
+import { Course } from 'src/app/modules/models/course';
+import { CoursesService } from 'src/app/modules/services/courses.service';
 
 @Component({
   selector: 'app-course-modal',
   templateUrl: './course-modal.component.html',
   styleUrls: ['./course-modal.component.scss']
 })
-export class CourseModalComponent implements OnInit {
+export class CourseModalComponent implements OnInit, OnDestroy {
   
+  coursesSubscription!: Subscription;
   form!: FormGroup;
 
-  constructor(private fb: FormBuilder) { }
+  constructor(
+    private readonly fb: FormBuilder,
+    private readonly coursesService: CoursesService
+  ) { }
 
   ngOnInit(): void {
     this.form = this.fb.group({
@@ -24,6 +31,17 @@ export class CourseModalComponent implements OnInit {
   }
 
   addCourse() {
-    console.log(this.form.controls['courseName'].value)
+    const course: Course = {
+      Name: this.form.controls['courseName'].value,
+      CourseNumber: this.form.controls['courseNumber'].value,
+      Points: this.form.controls['coursePoints'].value,
+      Type: this.form.controls['courseType'].value
+    }
+    
+    this.coursesSubscription = this.coursesService.postCourse(course).subscribe(res => {});
+  }
+
+  ngOnDestroy() {
+    this.coursesSubscription.unsubscribe();
   }
 }
