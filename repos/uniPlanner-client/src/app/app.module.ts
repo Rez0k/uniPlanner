76f0438d588a55c9468c75650a8b3f50cd40c9b5
Yoram Izilov <izilovy@gmail.com>
2022-05-12 11:45:56 +0300

    new component dropdown

--- repos/uniPlanner-client/src/app/app.module.ts
+++ repos/uniPlanner-client/src/app/app.module.ts
@@ -4,10 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { UniversityDropdownComponent } from './modules/components/university-dropdown/university-dropdown.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UniversityDropdownComponent
   ],
   imports: [
     BrowserModule,
---
+++ repos/uniPlanner-client/src/app/modules/components/university-dropdown/university-dropdown.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-university-dropdown',
+  templateUrl: './university-dropdown.component.html',
+  styleUrls: ['./university-dropdown.component.scss']
+})
+export class UniversityDropdownComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
