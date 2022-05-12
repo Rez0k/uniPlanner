af4fa1b1987ea57c5e2a8d01487a39867dc140eb
Ofek Ifergan <ofekifergan@gmail.com>
2022-05-12 10:56:30 +0300

    fag

--- repos/uniPlanner-server/src/controllers/data/data.controller.ts
+++ repos/uniPlanner-server/src/controllers/data/data.controller.ts
@@ -8,28 +8,16 @@ import {
 } from '@nestjs/common';
 import { Response } from 'express';
 
-@Controller('data')
+@Controller('api/courses')
 export class DataController {
-  @Get('d')
-  getHello(@Res() res: Response): void {
-    Logger.log('Got request to get data');
-    res.setHeader('Content-Type', 'text/html; charset=utf-8');
-    res.setHeader('Transfer-Encoding', 'chunked');
-
-    let num = 1;
-    const interval = setInterval(() => {
-      res.write('Hayush ' + num);
-      num++;
-      if (num > 10) {
-        res.end();
-        clearInterval(interval);
-      }
-    }, 1000);
+  @Get('key')
+  getCourseById(): string {
+    return 'Uriel the fag';
   }
 
-  @Get('h')
-  getHellog(): void {
-    Logger.log('Got request to get data');
-    throw new HttpException('Forbidden', HttpStatus.INTERNAL_SERVER_ERROR);
-  }
+  // @Get('h')
+  // getHellog(): void {
+  //   Logger.log('Got request to get data');
+  //   throw new HttpException('Forbidden', HttpStatus.INTERNAL_SERVER_ERROR);
+  // }
 }
