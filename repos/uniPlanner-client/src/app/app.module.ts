35b19ba9c4b257aac603505ca0f7b9a9bf245ec9
Eran Daniel <eran1eran2@gmail.com>
2022-05-12 11:54:46 +0300

    router fix by Ofek

--- repos/uniPlanner-client/src/app/app.module.ts
+++ repos/uniPlanner-client/src/app/app.module.ts
@@ -4,15 +4,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { CoursesTableComponent } from './modules/components/courses-table/courses-table.component';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  { path: '', component: CoursesTableComponent }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CoursesTableComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
