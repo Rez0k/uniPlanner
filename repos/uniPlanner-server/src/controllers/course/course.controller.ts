f29f2acffb26d95fb697d5b1b95318a8ba20559a
ss <ss@gmail.com>
2022-05-12 18:03:05 +0300

    update

--- repos/uniPlanner-server/src/controllers/course/course.controller.ts
+++ repos/uniPlanner-server/src/controllers/course/course.controller.ts
@@ -8,10 +8,10 @@ export class CourseController {
     constructor(readonly mongoService: MongoService) {}
 
     @Post('SaveCourse')
-    saveItemByCollection(@Body() course): string {
+    async saveItemByCollection(@Body() course): Promise<string> {
         try {
-            Logger.log('Post request to post data');
-            this.mongoService.saveItemByCollection("courses",  course);
+            Logger.log('Post request to post course data to collection');
+            await this.mongoService.saveItemByCollection("courses",  course);
             return `Course ${course.name} saved successfully`;
         } catch (error) {
             Logger.error(`Failed to post data, error: ${error}`)
@@ -19,10 +19,10 @@ export class CourseController {
         }
     }
 
-    @Get('allcourses')
+    @Get('allCourses')
     async getAllcourses(): Promise<Course[]> {
         try {
-            Logger.log('Got request to get all data');
+            Logger.log('Got request to get all collection data');
             const courses: Course[] = await this.mongoService.getAllByCollection("courses") as any;
             return courses;
         } catch (error) {
@@ -34,7 +34,7 @@ export class CourseController {
     @Get(':course_number')
     async getCourseByNumber(@Param() params): Promise<Course[]> {
         try {
-            Logger.log('Got request to get course data');
+            Logger.log('Got request to get course data from collection');
             const courses: Course[] = await this.mongoService.getItemByCourseNumber("courses",params.course_number) as any;
             return courses;
         } catch (error) {
