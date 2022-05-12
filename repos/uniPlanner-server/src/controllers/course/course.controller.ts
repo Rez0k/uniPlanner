dc626536369b4ff31588d32edaf3fb6994650529
Ofek Ifergan <ofekifergan@gmail.com>
2022-05-12 14:37:49 +0300

    update

--- repos/uniPlanner-server/src/controllers/course/course.controller.ts
+++ repos/uniPlanner-server/src/controllers/course/course.controller.ts
@@ -1,4 +1,14 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
+import { MongoService } from 'src/services/mongo.service';
 
-@Controller('api/course')
-export class CourseController {}
+@Controller('api/courses')
+export class CourseController {
+
+    constructor(readonly mongoService: MongoService) {}
+
+    @Get('key')
+    getCourseById(): string {
+        
+        return 'Uriel the fag';
+    }
+}
