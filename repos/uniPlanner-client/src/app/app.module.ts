1433d89ebaedc95383ef15a6c7be3da6caa402e9
Eran Daniel <eran1eran2@gmail.com>
2022-05-12 12:00:34 +0300

    chage courses components path

--- repos/uniPlanner-client/src/app/app.module.ts
+++ repos/uniPlanner-client/src/app/app.module.ts
@@ -4,7 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { CoursesTableComponent } from './modules/components/courses-table/courses-table.component';
+import { CoursesTableComponent } from './modules/components/courses/courses-table/courses-table.component';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
