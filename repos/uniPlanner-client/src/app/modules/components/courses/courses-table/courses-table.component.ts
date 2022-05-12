6a1c1a9ded03f880bb56ced72ca052cc83aabe22
uriel07777 <uriel07777@gmail.com>
2022-05-12 19:46:14 +0300

    Added services.

--- repos/uniPlanner-client/src/app/modules/components/courses/courses-table/courses-table.component.ts
+++ repos/uniPlanner-client/src/app/modules/components/courses/courses-table/courses-table.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Course } from 'src/app/modules/models/course';
+import { CurriculumDetailsModel } from 'src/app/modules/models/curriculumDetails';
+import { CoursesService } from 'src/app/modules/services/courses.service';
+
 @Component({
   selector: 'app-courses-table',
   templateUrl: './courses-table.component.html',
@@ -7,9 +11,45 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CoursesTableComponent implements OnInit {
 
-  constructor() { }
+  public courses: any[] = [];
 
-  ngOnInit(): void {
+  public curriculumDetails: any[] = [];
+
+  constructor(readonly coursesService: CoursesService,) { }
+
+  ngOnInit(): void 
+  {
+    this.coursesService.getAllCourses()
+      .subscribe((rows) => {this.courses = rows;
+      console.log('courses', this.courses);
+      });
+
+    this.curriculumDetails=[
+      {
+        Semester:  1,
+        Year: 2022,
+        Name: 'infi',
+        CourseNumber: 0,
+        Points:  0,
+        Level:  0,
+        Type: '',
+        Grade:  0,
+        Status: '',
+        UserName:  '0',
+      },
+      {
+        Semester:  0,
+        Year: 0,
+        Name: '',
+        CourseNumber: 0,
+        Points:  0,
+        Level:  0,
+        Type: '',
+        Grade:  0,
+        Status: '',
+        UserName:  'dsd',
+      },
+    ]  
   }
 
 }
